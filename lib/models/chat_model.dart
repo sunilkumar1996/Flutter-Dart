20e59f4dea0645ae787449952920e2577311e268
sunilkumar1996 <sunilrajputdev@gmail.com>
2020-04-24 17:46:07 +0530

    Flutter whatsApp Application

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,53 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Sunil Kumar",
+      message: "Hey Flutter, You are so amazing !",
+      time: "15:30",
+      avatarUrl:
+          "https://www.shutterstock.com/image-photo/silhouette-man-hood-dark-mysterious-hoodie-756307072"),
+  new ChatModel(
+      name: "Boss",
+      message: "Hey Sir Good Morning!",
+      time: "9:30 am",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Sanjeev Sir",
+      message: "Hello G!",
+      time: "5:00 am",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Ravi",
+      message: "I'm good!",
+      time: "10:30 am",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Piyush",
+      message: "I'm the fastest man alive!",
+      time: "12:30 am",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Bevafa Sanam",
+      message: "Mar ja kahi jake!",
+      time: "15:30 pm",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Pappu",
+      message: "Hi !",
+      time: "23:00 pm",
+      avatarUrl: "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb" 
+  ),
+];
